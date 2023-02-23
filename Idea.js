eedb00ece47ae1e5b6abb4a78607e635206d2204
Carol Bradsen <carolbradsen@gmail.com>
2023-02-22 20:29:55 -0700

    feat:Add Idea class to begin javascript

---
+++ Idea.js
@@ -0,0 +1,20 @@
+class Idea {
+  constructor(title, body) {
+    this.id = Date.now();
+    this.title = "";
+    this.body = "";
+    this.star = false;
+
+  }
+  updateIdea() {
+    if (this.star === false) {
+      this.star = true;
+    } else {
+      this.star = false
+    }
+    //this will update the idea's starred state
+    //if it is outlined when the user clicks on it, 
+   // it becomes a filled star (favorite). 
+   // if it is filled it becomes outlined star (unfavoriting).
+  }
+}
