582c1be167874c41d3e92987656a2327675abaaa
Sara Park <sspark106@gmail.com>
2023-02-24 16:34:21 -0700

    feat: Remove card from array and DOM with clicking delete icon

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var cardBody = document.getElementById('card-text');
 var ideaBoxContainer = document.querySelector('.idea-boxes-container');
 
 form.addEventListener('input', hoverSaveBtn);
-
+ideaBoxContainer.addEventListener('click', deleteCard);
 saveBtn.addEventListener('click', function(event) {
     event.preventDefault();
     if (inputTitle.value && inputBody.value) {
@@ -39,31 +39,43 @@ function deactivateSaveBtn() {
 function saveIdea() {
     var newIdea = new Idea(inputTitle.value, inputBody.value);
     ideas.push(newIdea);
-    renderCard(newIdea);
+    renderCard();
 }
 
-function renderCard(idea) {
-    var ideacard = " ";
-    ideacard = 
-    `<article class="idea-box">
-        <div class="card-top" id="card-header">
-            <img src="./assets/star-active.svg" alt="red-star-icon" id="card-star">
-            <img src="./assets/delete.svg" alt="white-delete-icon" id="delete-icon">
-        </div>
-        <div class="card-body" id="text-box-card">
-            <h5>${idea.title}</h5>
-            <p id="card-text">${idea.body}</p>
-        </div>
-        <div class="card-bottom" id="comment">
-            <img src="./assets/comment.svg" alt="plus-icon" id="comment-plus-icon">
-            <p id="white-comment">Comment</p>
-        </div>    
-    </article>`
-    ideaBoxContainer.innerHTML += ideacard;
+function renderCard() {
+    ideaBoxContainer.innerHTML = ' ';
+    for (var i = 0; i < ideas.length; i++) {
+        ideaBoxContainer.innerHTML += 
+        `<article class="idea-box" id="${ideas[i].id}">
+            <div class="card-top" id="card-header">
+                <img src="./assets/star-active.svg" alt="red-star-icon" id="card-star">
+                <img src="./assets/delete.svg" alt="white-delete-icon" id="delete-icon">
+            </div>
+            <div class="card-body" id="text-box-card">
+                <h5>${ideas[i].title}</h5>
+                <p id="card-text">${ideas[i].body}</p>
+            </div>
+            <div class="card-bottom" id="comment">
+                <img src="./assets/comment.svg" alt="plus-icon" id="comment-plus-icon">
+                <p id="white-comment">Comment</p>
+            </div>    
+        </article>`
+    }
     clearInput();
 }
 
 function clearInput() {
     form.reset();
     deactivateSaveBtn();
+}
+
+function deleteCard(event) {
+    if (event.target.id === "delete-icon") {
+        for (var i = 0; i < ideas.length; i++) {
+            if (event.target.parentElement.parentElement.id === ideas[i].id.toString()) {
+                ideas.splice(i, 1);
+            }
+        }
+    }
+    renderCard();
 }
