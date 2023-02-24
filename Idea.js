f629b31088fc18fd973ab2bf3dd0bad8dd5dbc21
Sara Park <sspark106@gmail.com>
2023-02-23 17:34:18 -0700

    feat: create an instance of Idea, push to array, and add event listener to save button

--- Idea.js
+++ Idea.js
@@ -1,8 +1,8 @@
 class Idea {
   constructor(title, body) {
     this.id = Date.now();
-    this.title = "";
-    this.body = "";
+    this.title = title;
+    this.body = body;
     this.star = false;
 
   }
--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 var ideas = []
+var newIdea;
+
+var saveBtn = document.querySelector('.save-button');
+var title = document.getElementById('title-input');
+var body = document.getElementById('body-input');
+
+saveBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (title.value && body.value) {
+        saveIdea();
+    }
+});
+
+function saveIdea() {
+    var newIdea = new Idea(title.value, body.value);
+    ideas.push(newIdea);
+}
+//event listener of click on save button
+//event handler to save idea 
+    // make an new instance of Idea class 
+    // push idea to ideas array 
+    // render that card on the DOM (renderToDOM function)
+    // clear input fields (clearInputFields function)
+// save button should only work when body and title have input 
+// if undefined, save button is a lighter color and cursor does not turn into pointer 
+    // save button is darker color and cursor turns to pointer if body/title have input 
+// event prevent default  
